655ee0ffe91b40793072ac4ffbcdabbc28cb7897
SiddharthPant <spsiddharthpant@gmail.com>
2024-02-26 19:03:29 +0530

    Expand config of bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,21 @@
 return {
 	"akinsho/bufferline.nvim",
 	config = function()
-		require("bufferline").setup({})
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				numbers = "buffer_id",
+				middle_mouse_command = "bdelete! %d", -- can be a string | function, | false see "Mouse actions"
+				indicator = {
+					style = "underline",
+				},
+				diagnostics = "nvim_lsp",
+				-- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+				diagnostics_indicator = function(count, level, diagnostics_dict, context)
+					return "(" .. count .. ")"
+				end,
+				separator_style = "slant",
+			},
+		})
 	end,
 }
