8c95f8d8af97192f1dc0c7ce6f47702188ab8f93
SiddharthPant <spsiddharthpant@gmail.com>
2024-02-26 13:12:45 +0530

    telescope enable icons, keymaps and resume middleware

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -28,20 +28,20 @@ return {
 					"__pycache__/*",
 					"renv/*",
 				},
-				-- mappings = {
-				--   n = {
-				--     ['v'] = actions.file_vsplit,
-				--     ['<S-k>'] = actions.preview_scrolling_up,
-				--     ['<S-j>'] = actions.preview_scrolling_down,
-				--   },
-				--   i = {
-				--     ['<C-v>'] = actions.file_vsplit,
-				--     ['<S-k>'] = actions.preview_scrolling_up,
-				--     ['<S-j>'] = actions.preview_scrolling_down,
-				--     ['<C-j>'] = actions.move_selection_next,
-				--     ['<C-k>'] = actions.move_selection_previous,
-				--   },
-				-- },
+				mappings = {
+				  n = {
+				    ['v'] = actions.file_vsplit,
+				    ['<S-k>'] = actions.preview_scrolling_up,
+				    ['<S-j>'] = actions.preview_scrolling_down,
+				  },
+				  i = {
+				    ['<C-v>'] = actions.file_vsplit,
+				    ['<S-k>'] = actions.preview_scrolling_up,
+				    ['<S-j>'] = actions.preview_scrolling_down,
+				    ['<C-j>'] = actions.move_selection_next,
+				    ['<C-k>'] = actions.move_selection_previous,
+				  },
+				},
 			},
 			pickers = {
 				buffers = {
@@ -66,7 +66,7 @@ return {
 					initial_mode = "insert",
 					hidden = true,
 					no_ignore = false,
-					disable_devicons = true,
+					disable_devicons = false,
 				},
 			},
 			extensions = {
@@ -85,7 +85,7 @@ return {
 					path = "%:p:h",
 					cwd = "%:p:h",
 					respect_gitignore = false,
-					dir_icon = "",
+					-- dir_icon = "",
 				},
 			},
 		})
@@ -94,20 +94,41 @@ return {
 		require("telescope").load_extension("file_browser")
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
-		vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
-		vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "Registers" })
-		vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Document Symbols" })
-		vim.keymap.set("n", "<leader>fw", builtin.lsp_workspace_symbols, { desc = "Workspace Symbols" })
-		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Old Files" })
-		vim.keymap.set("n", "<leader>ft", builtin.filetypes, { desc = "File Types" })
-		vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Quickfix" })
-		vim.keymap.set("n", "<leader>fl", builtin.loclist, { desc = "Location List" })
-		vim.keymap.set("n", "<leader>f:", builtin.commands, { desc = "Commands" })
-		vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "Git Files" })
+
+		-- Middleware to resume picker command from last search
+		local _last_picker = nil
+		local _last_ctx = nil
+		local function telescope_middleware(func, ctxfunc)
+			local function inner()
+				local ctx = nil
+				if ctxfunc ~= nil then
+					ctx = ctxfunc()
+				end
+				if func == _last_picker and vim.deep_equal(ctx, _last_ctx) then
+					builtin.resume()
+				else
+					_last_picker = func
+					_last_ctx = ctx
+					func()
+				end
+			end
+			return inner
+		end
+
+		vim.keymap.set("n", "<leader>ff", telescope_middleware(builtin.find_files), { desc = "Find Files" })
+		vim.keymap.set("n", "<leader>fg", telescope_middleware(builtin.live_grep), { desc = "Live Grep" })
+		vim.keymap.set("n", "<leader>fb", telescope_middleware(builtin.buffers), { desc = "Buffers" })
+		vim.keymap.set("n", "<leader>fh", telescope_middleware(builtin.help_tags), { desc = "Help Tags" })
+		vim.keymap.set("n", "<leader>fc", telescope_middleware(builtin.commands), { desc = "Commands" })
+		vim.keymap.set("n", "<leader>fr", telescope_middleware(builtin.registers), { desc = "Registers" })
+		vim.keymap.set("n", "<leader>fs", telescope_middleware(builtin.lsp_document_symbols), { desc = "Document Symbols" })
+		vim.keymap.set("n", "<leader>fw", telescope_middleware(builtin.lsp_workspace_symbols), { desc = "Workspace Symbols" })
+		vim.keymap.set("n", "<leader>fo", telescope_middleware(builtin.oldfiles), { desc = "Old Files" })
+		vim.keymap.set("n", "<leader>ft", telescope_middleware(builtin.filetypes), { desc = "File Types" })
+		vim.keymap.set("n", "<leader>fq", telescope_middleware(builtin.quickfix), { desc = "Quickfix" })
+		vim.keymap.set("n", "<leader>fl", telescope_middleware(builtin.loclist), { desc = "Location List" })
+		vim.keymap.set("n", "<leader>f:", telescope_middleware(builtin.commands), { desc = "Commands" })
+		vim.keymap.set("n", "<leader>fp", telescope_middleware(builtin.git_files), { desc = "Git Files" })
 		vim.keymap.set("n", "<leader>fe", ":Telescope file_browser<CR>", { desc = "File Browser", noremap = true })
 	end,
 }
