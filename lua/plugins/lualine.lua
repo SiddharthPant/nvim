3618f3a62e21bcec74f9a5632e3d4c5d9a85ece1
SiddharthPant <spsiddharthpant@gmail.com>
2024-02-26 17:22:07 +0530

    Add lualine winbar config and extensions

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,13 +7,37 @@ return {
 				icons_enabled = true,
 			},
 			sections = {
-				lualine_c = { { "filename", file_status = true, path = 1 } }, -- 0 = just filename, 1 = relative path, 2 = absolute path
+				lualine_c = {},
 			},
 			inactive_sections = {
-				lualine_c = { { "filename", file_status = true, path = 1 } },
+				lualine_c = {},
 			},
-			tabline = {
-				lualine_a = { { "buffers" } },
+			tabline = {},
+			winbar = {
+				lualine_a = {},
+				lualine_b = { { "filename", file_status = false, path = 2 } }, -- 0 = just filename, 1 = relative path, 2 = absolute path
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			inactive_winbar = {
+				lualine_a = {},
+				lualine_b = { { "filename", file_status = false, path = 2 } }, -- 0 = just filename, 1 = relative path, 2 = absolute path
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			extensions = {
+				"trouble",
+				"fzf",
+				"nvim-dap-ui",
+				"fugitive",
+				"mason",
+				"lazy",
+				"man",
+				"quickfix",
 			},
 		})
 	end,
