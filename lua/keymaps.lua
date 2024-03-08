6dfe644bc0e5af0b9e8ec2bc6760ff9d5891e719
SiddharthPant <spsiddharthpant@gmail.com>
2024-03-08 15:23:00 +0530

    Expand conform config and keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -59,7 +59,6 @@ vim.keymap.set('n', '<leader>Y', [["+Y]])
 vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
 
 vim.keymap.set('n', 'Q', '<nop>')
-vim.keymap.set('n', '<leader>F', vim.lsp.buf.format)
 
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
