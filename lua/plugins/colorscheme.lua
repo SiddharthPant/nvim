667ad129f2119c09ec9b8b960ab770a12026256d
SiddharthPant <spsiddharthpant@gmail.com>
2024-03-08 08:42:50 +0000

    Make keywords bold

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,6 +8,19 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
+      require('tokyonight').setup {
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          -- comments = { italic = true },
+          keywords = { italic = true, bold = true },
+          -- functions = {},
+          -- variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          -- sidebars = "dark", -- style for sidebars, see below
+          -- floats = "dark", -- style for floating windows
+        },
+      }
       -- Load the colorscheme here
       vim.cmd.colorscheme 'tokyonight-night'
 
