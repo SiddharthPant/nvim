ed2013c014f0b295c1828d0ccbb98a21c029ae32
SiddharthPant <spsiddharthpant@gmail.com>
2024-03-13 18:47:30 +0530

    Add alpha.nvim and update plugins

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,22 @@
+return {
+  'goolord/alpha-nvim',
+  config = function()
+    local alpha = require 'alpha'
+    local theta = require 'alpha.themes.theta'
+    -- Set header
+    theta.header.val = {
+      '                                                     ',
+      '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+      '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+      '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+      '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+      '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+      '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+      '                                                     ',
+    }
+    theta.header.opts.hl = 'AlphaHeader'
+
+    -- Send config to alpha
+    alpha.setup(theta.config)
+  end,
+}
